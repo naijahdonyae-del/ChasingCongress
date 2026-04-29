f35d3b471a82a20c8be67cfdbf4bb8a5bbf440cb
Ninja2270 <Monts2fj@gmail.com>
2026-04-29 13:39:02 -0400

    Revert "Fixing errors"
    This reverts commit 7d34fc4e1da9d9f62b8c25ee92303db85f88f0e7.

--- My project/Assets/Mytest/Trigger.cs
+++ My project/Assets/Mytest/Trigger.cs
@@ -1,52 +1,28 @@
-using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Trigger : MonoBehaviour
 {
     public Timer timer;
 
-    [SerializeField] TextMeshProUGUI timerText;
-    [SerializeField] float remainingTime;
-    [SerializeField] bool timeIsRunning = true;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
 
+        if (sceneName == "SampleScene")
+        {
+            timer.enabled = true;
+        }
+        else if(sceneName == "TitleScreen")
+        {
+            timer.enabled = false;
+        }
+
+    }
 
     // Update is called once per frame
-    private void Awake()
-    {
-        ResetTimer();
-        timeIsRunning = true;
-        timerText.color = Color.white;
-
-
-    }
     void Update()
     {
-        if (timeIsRunning && remainingTime > 0)
-        {
-            remainingTime -= Time.deltaTime;
-
-
-
-
-        }
-        if(remainingTime == 0)
-        {
-
-            timeIsRunning = false;
-
-            SceneManager.LoadScene(0);
-
-            ResetTimer();
-
-        }
-
-        int minutes = Mathf.FloorToInt(remainingTime / 60);
-        int seconds = Mathf.FloorToInt(remainingTime % 60);
-        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-    }
-    void ResetTimer()
-    {
-        remainingTime = 10;
-        timeIsRunning = false;
+        
     }
 }
