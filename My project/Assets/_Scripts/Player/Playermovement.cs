a12b3dd61c68338d788a3297eb803a0b45c08e31
Jorge-7-X <jel367@pitt.edu>
2026-05-15 04:34:09 -0400

    Fixed Player Animations

--- My project/Assets/_Scripts/Player/Playermovement.cs
+++ My project/Assets/_Scripts/Player/Playermovement.cs
@@ -7,11 +7,13 @@ public class Playermovement : MonoBehaviour
     private Rigidbody2D rb;
 
     private Vector2 _moveDirection;
+    private Vector2 _currInput;
 
     public InputActionReference move;
     public InputActionReference interact; //For dialogue and other options
 
     [SerializeField] private Animator _animator;
+    private string lastDirection = "Down";
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -24,16 +26,62 @@ public class Playermovement : MonoBehaviour
     void Update()
     {
         _moveDirection = move.action.ReadValue<Vector2>();
-        if (_moveDirection != Vector2.zero)
+        HandleAnim();
+    }
+
+    private void HandleAnim()
+    {
+        if (_animator == null) return;
+
+        string animationName = "";
+
+        if (_moveDirection == Vector2.zero)
         {
-            _animator.SetBool("Onrun", true);
+            animationName = "Idle";
         }
         else
         {
-            _animator.SetBool("Onrun", false);
+            animationName = "Walking";
+        }
+        _animator.Play(animationName + lastDirection);
+    }
 
+    private Vector3 GetDirection(Vector3 input)
+    {
+        Vector3 finalDirection = Vector3.zero;
+
+        if(input.y > 0.1f)
+        {
+            lastDirection = "Up";
+            finalDirection = new Vector2(0, 1);
+        }
+        else if (input.y < -0.1f)
+        {
+            lastDirection = "Down";
+            finalDirection = new Vector2(0, -1);
+        }
+        else if (input.x > 0.1f)
+        {
+            lastDirection = "Right";
+            finalDirection = new Vector2(1, 0);
+        }
+        else if (input.x < -0.1f)
+        {
+            lastDirection = "Left";
+            finalDirection = new Vector2(-1, 0);
+        }
+        else
+        {
+            finalDirection = Vector2.zero;
         }
 
+        return finalDirection;
+    }
+
+    private void OnMove(InputValue value)
+    {
+        _currInput = value.Get<Vector2>().normalized;
+        _moveDirection = GetDirection(_currInput);
     }
 
     public void FixedUpdate()
